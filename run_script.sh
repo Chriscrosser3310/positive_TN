0de50d03884543e1775c8210d3e048718e711ff5
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-06 18:22:04 -0700

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=1-00:00:00
+#SBATCH -J QTT_HF_AR
+##SBATCH -o job.out
+##SBATCH -e job.err
+#SBATCH --mail-user=jchen9@caltech.edu
+#SBATCH --mail-type=all
+#SBATCH --partition=serial,parallel
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH -x pauling[001-002]
+#SBATCH --mem=100G
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+# mkdir /scratch/local/Chriscrosser/
+for u in 8
+do
+    echo $u
+    python positive_TN_script.py 2 [10,20,30] 10 10
+done
