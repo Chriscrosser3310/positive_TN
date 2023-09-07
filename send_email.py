7114fae7ecf12011438df5aaae44c64c1accc57f
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-06 19:41:55 -0700

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,4 +7,5 @@ with open(script_directory + '\\sbatch_buffer.txt', 'r', encoding='utf-16') as f
     last_line = f.read()
 while 'done' not in last_line:
     os.system("mail -s 'positive_TN' jchen9@caltech.edu < sbatch_buffer.txt")
+    print("email sent")
     sleep(60)
