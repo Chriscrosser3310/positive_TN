30751433a17a4432af0b72119ecf5c24ded60ac2
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-07 01:08:11 -0700

    add multiple scripts

---
+++ all_one/d=3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=10-00:00:00
+#SBATCH -J positive_TN
+##SBATCH -o job.out
+##SBATCH -e job.err
+#SBATCH --mail-user=jchen9@caltech.edu
+#SBATCH --mail-type=all
+#SBATCH --partition=serial,parallel
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH -x pauling[001-002]
+#SBATCH --mem=100G
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+# mkdir /scratch/local/Chriscrosser/
+
+python -u all_one_script.py 3 [10,15,20] 10 50 > 3_[10,15,20]_10_50.txt
