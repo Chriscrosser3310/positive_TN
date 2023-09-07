db460b6c6c6286f2b970edad0ebc2304bf3b35bf
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-06 19:47:43 -0700

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import sys
 from time import sleep
 
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-with open(script_directory + '\\sbatch_buffer.txt', 'r', encoding='utf-16') as f:
+with open(script_directory + '/sbatch_buffer.txt', 'r') as f:
     last_line = f.read()
 while 'done' not in last_line:
     os.system("mail -s 'positive_TN' jchen9@caltech.edu < sbatch_buffer.txt")
