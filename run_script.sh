d01f7010aa0ba4af18c9a35e4d446469501c03d1
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-07 00:49:22 -0700

    update run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --time=10-00:00:00
+#SBATCH -J positive_TN
+##SBATCH -o job.out
+##SBATCH -e job.err
+#SBATCH --mail-user=jchen9@caltech.edu
+#SBATCH --mail-type=all
+#SBATCH --partition=serial,parallel
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH -x pauling[001-002]
+#SBATCH --mem=100G
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+# mkdir /scratch/local/Chriscrosser/
+
+srun --exclusive --ntasks=1 python -u positive_TN_script.py 2 [10,20,30] 10 50 > 2_[10,15,20]_10_50.txt
+srun --exclusive --ntasks=1 python -u positive_TN_script.py 3 [10,20,30] 10 50 > 3_[10,15,20]_10_50.txt
+srun --exclusive --ntasks=1 python -u positive_TN_script.py 4 [10,20,30] 10 50 > 4_[10,15,20]_10_50.txt
+wait
