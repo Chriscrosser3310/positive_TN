cb2e7517a4832042ec5a8f0d96a90d9c19fb3572
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-06 18:29:59 -0700

    No print

--- positive_TN_script.py
+++ positive_TN_script.py
@@ -9,7 +9,7 @@ nlist = eval(sys.argv[2])
 plist = np.linspace(0, 2/d**3, int(sys.argv[3]), endpoint=True)
 repeat = int(sys.argv[4])
 
-avg_table, std_table = avg_entropy_nplist(nlist, plist, bdim=d, repeat=repeat, mode="all_one", prt=True)
+avg_table, std_table = avg_entropy_nplist(nlist, plist, bdim=d, repeat=repeat, mode="all_one", prt=False)
 
 with open(npz_directory, 'wb') as f:
     np.savez(f, d=[d], nlist=nlist, plist=plist, avg_table=avg_table, std_table=std_table)
