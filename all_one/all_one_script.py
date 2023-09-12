763fb230bbf105f1af96de4ae6e3005197fad4d6
Jielun (Chris) Chen <3042109025@qq.com>
2023-09-11 18:57:23 -0700

    include width_mode param

--- all_one/all_one_script.py
+++ all_one/all_one_script.py
@@ -8,15 +8,16 @@ d = int(sys.argv[1])
 nlist = eval(sys.argv[2])
 plist = np.linspace(0, 2/d**3, int(sys.argv[3]), endpoint=True)
 repeat = int(sys.argv[4])
+width_mode = sys.argv[5]
 
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-npz_name = f'{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}'
+npz_name = f'{sys.argv[1]}_{sys.argv[2]}_{sys.argv[3]}_{sys.argv[4]}_{sys.argv[5]}'
 npz_directory = script_directory + f'/{npz_name}.npz'
 with open(npz_directory, 'a+') as f:
     pass
 print(npz_directory)
 
-avg_table, std_table = avg_entropy_nplist(nlist, plist, bdim=d, repeat=repeat, mode="all_one", prt=True, filename=npz_name)
+avg_table, std_table = avg_entropy_nplist(nlist, plist, bdim=d, repeat=repeat, mode="all_one", prt=True, filename=npz_name, width_mode=width_mode)
 with open(npz_directory, 'wb') as f:
     np.savez(f, d=[d], nlist=nlist, plist=plist, avg_table=avg_table, std_table=std_table)
 print("done")
